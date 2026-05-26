56e1cc5d331d5c825478d70c20b94bc61fc2ed11
Rohit Mahajan <ROHIT.MAHAJAN32@svkmmumbai.onmicrosoft.com>
2026-05-26 17:15:50 +0530

    Initial commit

---
+++ client/src/services/historyService.js
@@ -0,0 +1,17 @@
+export const getHistory = async () => {
+  return {
+    entries: [],
+  };
+};
+
+export const deleteHistoryItem = async () => {
+  return {
+    success: true,
+  };
+};
+
+export const clearHistory = async () => {
+  return {
+    success: true,
+  };
+};
